b3153188338509e65ccfdc3485d7366fdf8051e8
vlitvinova <vlitvinova95@gmail.com>
2020-04-29 02:41:22 +0300

    modified changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
-  production: false
+  production: false,
+  GIT_SEARCH_API_BASE_URL: 'https://api.github.com/search/repositories',
+  GIT_SEARCH_API_HEADER: new HttpHeaders().append('Accept', 'application/vnd.github.mercy-preview+json'),
+  paramsQuery: '?q=',
+  sortQuery: '&sort=stars',
+  pageQuery: '&page='
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
